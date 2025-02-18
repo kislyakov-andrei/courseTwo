216d95f80165d87ae5c1efaa883cfc348804cf59
kislyakov-andrei <kisli@mail.ru>
2025-02-18 18:40:22 +0300

    homeWork7 chaned

--- src/org/skypro/skyshop/product/LenthStringComparator.java
+++ src/org/skypro/skyshop/product/LenthStringComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
     public class LenthStringComparator implements Comparator<Searchable> {
         @Override
         public int compare(Searchable o1, Searchable o2) {
-            int lengthComparison = Integer.compare(o2.length(), o1.length());
+            int lengthComparison = Integer.compare(o2.getSearchTerm().length(), o1.getSearchTerm().length());
 
             // Если длины равны, сравниваем имена
             if (lengthComparison == 0) {
